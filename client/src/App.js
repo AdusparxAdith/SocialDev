ca4c693f09df113b600913d4b96bad6b4bb8bab9
AdusparxAdith <adithdinesh97@gmail.com>
2019-06-19 15:15:46 +0530

    Added profile form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
@@ -39,6 +40,12 @@ const App = () => {
                 exact
                 path="/dashboard"
                 component={Dashboard}
+              />
+              <PrivateRoute
+                Route
+                exact
+                path="/create-profile"
+                component={CreateProfile}
               />
             </Switch>
           </section>
