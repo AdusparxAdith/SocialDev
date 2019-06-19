014fb1774b6731f6d9a7ef50da72552bedae716d
AdusparxAdith <adithdinesh97@gmail.com>
2019-06-19 21:09:05 +0530

    Added  Delete experience,account,education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator/check");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 //@route GET api/profile
 //@desc Get all profiles
@@ -145,7 +146,8 @@ router.post(
 
 router.delete("/", auth, async (req, res) => {
   try {
-    // @todo - Remove posts
+    //Remove user posts
+    await Post.deleteMany({ user: req.user.id });
     //Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     await User.findOneAndRemove({ _id: req.user.id });
