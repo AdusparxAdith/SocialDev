680561f10c9f0b5418b9eb0dd97b6c95bf492135
AdusparxAdith <adithdinesh97@gmail.com>
2019-06-19 12:02:53 +0530

    Added Private route for Private pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -31,6 +34,12 @@ const App = () => {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <PrivateRoute
+                Route
+                exact
+                path="/dashboard"
+                component={Dashboard}
+              />
             </Switch>
           </section>
         </Fragment>
