ac4f58af1f23384a13081dd57807b02224cdc525
AdusparxAdith <adithdinesh97@gmail.com>
2019-06-19 13:00:30 +0530

    Added profile redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 //Component calls action, action does something and sends result to reducer, reducer returns state to component store
 export default combineReducers({
   alert,
-  auth
+  auth,
+  profile
 });
