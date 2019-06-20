34a4101e0905d03d30d5d70d02e20b34c757bf12
AdusparxAdith <adithdinesh97@gmail.com>
2019-06-20 15:22:29 +0530

    Added adding posts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,6 +11,7 @@ export const GET_PROFILE = "GET_PROFILE";
 export const GET_REPOS = "GET_REPOS";
 export const GET_POSTS = "GET_POSTS";
 export const DELETE_POST = "DELETE_POST";
+export const ADD_POST = "ADD_POST";
 export const POST_ERROR = "POST_ERROR";
 export const GET_PROFILES = "GET_PROFILES";
 export const PROFILE_ERROR = "PROFILE_ERROR";
