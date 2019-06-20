75b80f8b061938ce2c17e2371bad61e696257780
AdusparxAdith <adithdinesh97@gmail.com>
2019-06-20 19:52:20 +0530

    Added 404 page, redesigned routing

---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NotFound from "../layout/NotFound";
+import Dashboard from "../dashboard/Dashboard";
+import CreateProfile from "../profile-forms/CreateProfile";
+import EditProfile from "../profile-forms/EditProfile";
+import AddExperience from "../profile-forms/AddExperience";
+import AddEducation from "../profile-forms/AddEducation";
+import Profiles from "../profiles/Profiles";
+import Profile from "../profile/Profile";
+import PrivateRoute from "../routing/PrivateRoute";
+import Login from "../auth/Login";
+import Posts from "../posts/Posts";
+import Post from "../post/Post";
+import Register from "../auth/Register";
+import Alert from "../layout/Alert";
+
+const Routes = () => {
+  return (
+    <section className="container">
+      <Alert />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <PrivateRoute Route exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute
+          Route
+          exact
+          path="/create-profile"
+          component={CreateProfile}
+        />
+        <PrivateRoute
+          Route
+          exact
+          path="/edit-profile"
+          component={EditProfile}
+        />
+        <PrivateRoute
+          Route
+          exact
+          path="/add-experience"
+          component={AddExperience}
+        />
+        <PrivateRoute
+          Route
+          exact
+          path="/add-education"
+          component={AddEducation}
+        />
+        <PrivateRoute Route exact path="/posts" component={Posts} />
+        <PrivateRoute Route exact path="/posts/:id" component={Post} />
+        <Route component={NotFound} />
+      </Switch>
+    </section>
+  );
+};
+
+export default Routes;
